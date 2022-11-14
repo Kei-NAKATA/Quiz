f7a51d09b16c5f8de30c69664fd5e2c89077acb8
Kei NAKATA <s1f102002390@iniad.org>
2022-11-14 15:03:45 +0900

    スタート画面と卵割ゲームの追加
    スタート画面と卵割ゲームのシーンを追加しました。スタート画面から卵割ゲームへ遷移するように設定しています。

---
+++ Assets/Nakata/EggDirector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggDirector : MonoBehaviour
+{
+    int hp = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hp > 0){
+                hp = hp - 1;
+            }
+        }
+
+        if (hp == 0)
+        {
+            Sprite gazo = Resources.Load<Sprite>("egg2");
+            GameObject.Find("Canvas/Image").GetComponent<UnityEngine.UI.Image>().sprite = gazo;
+        }
+    }
+}
---
+++ Assets/Nakata/StartDirector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartDirector : MonoBehaviour
+{
+    //’·‰Ÿ‚µ‚Æ”»’è‚·‚éŽžŠÔ‚ðŠÇ—
+    float span = 2.0f;
+    //’·‰Ÿ‚µ‚µ‚Ä‚¢‚éƒtƒŒ[ƒ€”‚ð‹L˜^
+    float delta = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            this.delta += Time.deltaTime;
+        }
+
+        if (this.delta > this.span)
+        {
+            SceneManager.LoadScene("EggScene");
+        }
+    }
+}
