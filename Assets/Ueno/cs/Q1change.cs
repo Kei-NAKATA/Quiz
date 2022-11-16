c999c324236cf8d5687ebe09c3272d46b669f5e8
Masaya <s1f102000815@iniad.org>
2022-11-16 14:17:02 +0900

    シーンチェンジ機能の追加
    セレクト画面からEggSceneとIceSceneに行けるようになりました。

---
+++ Assets/Ueno/cs/Q1change.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Q1change : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        Debug.Log("ok.");
+        SceneManager.LoadScene("EggScene");
+    }
+}
