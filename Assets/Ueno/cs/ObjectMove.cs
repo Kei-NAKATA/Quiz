22150e45a5d52e191098999ece081a6ed3bb34bd
Masaya <s1f102000815@iniad.org>
2022-12-07 14:24:56 +0900

    ロープの修正、タッチエフェクトの実装(現段階では自分の)担当箇所のみ)、シーンチェンジ11~16
    ・ロープの上方向への移動制限を追加
    ・ロープを一定箇所まで下げるとシーンが切り替わるように変更
    ・11~16までのシーンチェンジを実装
    ・タッチエフェクトの実装(今はチュートリアルエリアのみ)

--- Assets/Ueno/cs/ObjectMove.cs
+++ Assets/Ueno/cs/ObjectMove.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectMove : MonoBehaviour
 {
+
     void Start()
     {
         //Rigidbodyを取得
-        var rb = GetComponent<Rigidbody>();
+        var rb = GetComponent<Rigidbody2D>();
 
         //FreezePositionXをオンにする
-        rb.constraints = RigidbodyConstraints.FreezePositionX;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
     }
+
     void OnMouseDrag()
     {
         //マウスの座標を取得してスクリーン座標を更新
@@ -21,11 +24,16 @@ public class ObjectMove : MonoBehaviour
         worldPosition.x = -1.66f;
         worldPosition.y += 2.6f;
 
+        if (worldPosition.y >= 3.54f)
+        {
+            worldPosition.y = 3.54f;
+        }
+
         if (worldPosition.y <= 2.58f)
         {
             worldPosition.y = 2.58f;
+            SceneManager.LoadScene("tutoriall13");
         }
-
         this.transform.position = worldPosition;
     }
 }
