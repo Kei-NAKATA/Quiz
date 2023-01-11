0bc757177dbf577494cc273608bd3c63f8204444
Masaya <s1f102000815@iniad.org>
2023-01-11 14:19:49 +0900

    チュートリアル完成
    チュートリアル完成しました。変更点もあるので書き並べていきます。
    ・スワイプの説明の削除
    　そもそもUnityでの実装が難しいことと、クイズにスワイプを使わないことから混乱を避けるため
    ・StageSelectにチュートリアルに戻るためのボタンを追加
    　タイトルからチュートリアルへの遷移は手をつけていないので注意
    ・「戻る」ボタンのデザインの修正
    　少し丸くなりました。
    ・ドラッグチュートリアルに下矢印を追加

--- Assets/Ueno/cs/GoTutoriall.cs
+++ Assets/Ueno/cs/GoTutoriall.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class tutoriall13 : MonoBehaviour
+public class GoTutoriall : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnClick()
     {
         Debug.Log("ok.");
-        SceneManager.LoadScene("tutoriallOther1");
+        SceneManager.LoadScene("tutoriall");
     }
 }
